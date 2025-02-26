fd88a2b0115c406a133a31fa8ea25e41088e17f2
Nethmi Ranasinghe <nethmisudeni@gmail.com>
2025-02-26 17:53:11 +0530

    Fix validate mandatory props only for APIs

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -3326,15 +3326,18 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
                                                         Map<String, Boolean> checklist) throws APIManagementException{
         APIStateChangeResponse response = new APIStateChangeResponse();
 
-        // validate custom API properties
-        if (StringUtils.equals(action, APIConstants.LC_PUBLISH_LC_STATE)) {
-            org.json.simple.JSONArray customProperties = APIUtil.getCustomProperties(this.username);
-            List<String> errorProperties = APIUtil.validateMandatoryProperties(customProperties,
-                    apiTypeWrapper.getApi().getAdditionalProperties());
-            if (!errorProperties.isEmpty()) {
-                String errorString = " : " + String.join(", ", errorProperties);
-                throw new APIManagementException(errorString, ExceptionCodes.from(ExceptionCodes
-                        .ERROR_WHILE_UPDATING_MANDATORY_PROPERTIES));
+        if (!apiTypeWrapper.isAPIProduct()){
+            // validate custom API properties
+            if (StringUtils.equals(action, APIConstants.LC_PUBLISH_LC_STATE)) {
+                org.json.simple.JSONArray customProperties = APIUtil.getCustomProperties(this.username);
+                List<String> errorProperties = APIUtil.validateMandatoryProperties(customProperties,
+                        apiTypeWrapper.getApi().getAdditionalProperties());
+
+                if (!errorProperties.isEmpty()) {
+                    String errorString = " : " + String.join(", ", errorProperties);
+                    throw new APIManagementException(errorString, ExceptionCodes.from(ExceptionCodes
+                            .ERROR_WHILE_UPDATING_MANDATORY_PROPERTIES));
+                }
             }
         }
 
