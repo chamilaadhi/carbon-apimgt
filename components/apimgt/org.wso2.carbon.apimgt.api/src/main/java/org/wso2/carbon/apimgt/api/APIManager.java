c4a6d3a8bbc15b9013deeeea33e057a557183963
Chamila <wso2@chamilaa.local>
2025-04-02 18:04:01 +0530

    Add org id to application property bag

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIManager.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIManager.java
@@ -277,6 +277,15 @@ public interface APIManager {
      * @throws org.wso2.carbon.apimgt.api.APIManagementException if failed add subscriber
      */
     void addSubscriber(String username, String groupingId) throws APIManagementException;
+    
+    /**
+     * Creates a new subscriber given the username, the grouping Id and organization
+     *
+     * @param username   Username of the subscriber to be added
+     * @param groupingId - the groupId to which the subscriber belongs to
+     * @throws org.wso2.carbon.apimgt.api.APIManagementException if failed add subscriber
+     */
+    void addSubscriber(String username, String groupingId, String organization) throws APIManagementException;
 
     /**
      * Updates the details of the given subscriber.
