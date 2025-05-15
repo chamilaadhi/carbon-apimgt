a69fa9cee6257bf54b2405083cfe850fda4784ae
Thisal Tennakoon <thisaltennakoon@gmail.com>
2025-05-15 18:43:22 +0530

    Make previous classes as deprecated
    in org.wso2.carbon.apimgt.impl.definitions

---
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/AsyncApiParserUtil.java
@@ -0,0 +1,8 @@
+package org.wso2.carbon.apimgt.impl.definitions;
+
+/**
+ * @deprecated use org.wso2.carbon.apimgt.spec.parser.definitions.AsyncApiParserUtil instead
+ */
+@Deprecated
+public class AsyncApiParserUtil extends org.wso2.carbon.apimgt.spec.parser.definitions.AsyncApiParserUtil {
+}
