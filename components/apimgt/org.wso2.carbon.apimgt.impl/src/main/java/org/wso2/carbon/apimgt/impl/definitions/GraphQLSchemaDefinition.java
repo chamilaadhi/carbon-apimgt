d5c3509230572ba64b0c881ed1ffb2767d95b7c0
Thisal Tennakoon <thisaltennakoon@gmail.com>
2025-05-15 21:31:10 +0530

    Revert "Remove unused functions"

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/GraphQLSchemaDefinition.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/GraphQLSchemaDefinition.java
@@ -19,9 +19,63 @@
 
 package org.wso2.carbon.apimgt.impl.definitions;
 
+import org.apache.commons.io.IOUtils;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.api.model.APIIdentifier;
+import org.wso2.carbon.apimgt.api.model.APIRevision;
+import org.wso2.carbon.apimgt.impl.APIConstants;
+import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
+import org.wso2.carbon.registry.api.Registry;
+import org.wso2.carbon.registry.api.RegistryException;
+import org.wso2.carbon.registry.api.Resource;
+import org.wso2.carbon.registry.core.RegistryConstants;
+
+import java.io.IOException;
+
 /**
  * @deprecated use org.wso2.carbon.apimgt.spec.parser.definitions.GraphQLSchemaDefinition instead
  */
 @Deprecated
 public class GraphQLSchemaDefinition extends org.wso2.carbon.apimgt.spec.parser.definitions.GraphQLSchemaDefinition {
+
+    /**
+     * Returns the graphQL content in registry specified by the wsdl name
+     *
+     * @param apiId Api Identifier
+     * @return graphQL content matching name if exist else null
+     */
+    public String getGraphqlSchemaDefinition(APIIdentifier apiId, Registry registry) throws APIManagementException {
+        String apiName = apiId.getApiName();
+        String apiVersion = apiId.getVersion();
+        String apiProviderName = apiId.getProviderName();
+        APIRevision apiRevision = ApiMgtDAO.getInstance().checkAPIUUIDIsARevisionUUID(apiId.getUUID());
+        String resourcePath;
+        if (apiRevision != null && apiRevision.getApiUUID() != null) {
+            resourcePath = APIUtil.getRevisionPath(apiRevision.getApiUUID(), apiRevision.getId());
+        } else {
+            resourcePath = APIUtil.getGraphqlDefinitionFilePath(apiName, apiVersion, apiProviderName);
+        }
+
+        String schemaDoc = null;
+        String schemaName = apiId.getProviderName() + APIConstants.GRAPHQL_SCHEMA_PROVIDER_SEPERATOR +
+                apiId.getApiName() + apiId.getVersion() + APIConstants.GRAPHQL_SCHEMA_FILE_EXTENSION;
+        String schemaResourePath = resourcePath + schemaName;
+        try {
+            if (registry.resourceExists(schemaResourePath)) {
+                Resource schemaResource = registry.get(schemaResourePath);
+                schemaDoc = IOUtils.toString(schemaResource.getContentStream(),
+                        RegistryConstants.DEFAULT_CHARSET_ENCODING);
+            }
+        } catch (RegistryException e) {
+            String msg = "Error while getting schema file from the registry " + schemaResourePath;
+            log.error(msg, e);
+            throw new APIManagementException(msg, e);
+        } catch (IOException e) {
+            String error = "Error occurred while getting the content of schema: " + schemaName;
+            log.error(error);
+            throw new APIManagementException(error, e);
+        }
+        return schemaDoc;
+    }
 }
--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OAS2Parser.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OAS2Parser.java
@@ -19,9 +19,40 @@
 
 package org.wso2.carbon.apimgt.impl.definitions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
+import io.swagger.util.Yaml;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.api.ExceptionCodes;
+import org.wso2.carbon.apimgt.impl.APIConstants;
+
 /**
  * @deprecated use org.wso2.carbon.apimgt.spec.parser.definitions.OAS2Parser instead
  */
 @Deprecated
 public class OAS2Parser extends org.wso2.carbon.apimgt.spec.parser.definitions.OAS2Parser {
+
+    /**
+     * Remove x-wso2-examples from all the paths from the swagger.
+     *
+     * @param swaggerString Swagger as String
+     */
+    public String removeExamplesFromSwagger(String swaggerString) throws APIManagementException {
+        try {
+            SwaggerParser swaggerParser = new SwaggerParser();
+            Swagger swagger = swaggerParser.parse(swaggerString);
+            swagger.getPaths().values().forEach(path -> {
+                path.getOperations().forEach(operation -> {
+                    if (operation.getVendorExtensions().keySet().contains(APIConstants.SWAGGER_X_EXAMPLES)) {
+                        operation.getVendorExtensions().remove(APIConstants.SWAGGER_X_EXAMPLES);
+                    }
+                });
+            });
+            return Yaml.pretty().writeValueAsString(swagger);
+        } catch (JsonProcessingException e) {
+            throw new APIManagementException("Error while removing examples from OpenAPI definition", e,
+                    ExceptionCodes.ERROR_REMOVING_EXAMPLES);
+        }
+    }
 }
